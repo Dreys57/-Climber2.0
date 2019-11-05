e1a2d2f11eab850d2b730e0d951239a714776e5f
Vincent <skriosdreys@gmail.com>
2019-11-05 16:08:52 +0100

    Adding the platformer, short of win condition and fall/reset

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Animator animator;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private GameObject platform;
+
+    private new Vector3 startPositions;
+    
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float jumpForce = 16.0f;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private float wallCheckDistance;
+    private float movementInputDirection;
+    
+
+    private bool isFacingRight = true;
+    private bool isWalking;
+    private bool isGrounded;
+    private bool canJump;
+    private bool isTouchingWall;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        startPositions = body.position;
+
+    }
+
+    void FixedUpdate()
+    {
+        ApplyMovement();
+        
+        CheckSurroundings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+
+        CheckMovementDirection();
+        
+        UpdateAnimations();
+        
+        CheckIfCanJump();
+
+        ResetPlayer();
+
+    }
+
+    private void CheckInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void CheckIfCanJump()
+    {
+        if (isGrounded && body.velocity.y < 0.1f || isTouchingWall)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+
+    private void Jump()
+    {
+        if (canJump)
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        body.velocity = new Vector2(speed * movementInputDirection, body.velocity.y);
+    }
+
+    private void UpdateAnimations()
+    {
+        animator.SetBool("isWalking", isWalking);
+        animator.SetBool("isGrounded", isGrounded);
+        animator.SetFloat("yVelocity", body.velocity.y);
+    }
+
+    void CheckMovementDirection()
+    {
+        if (isFacingRight && movementInputDirection < 0)
+        {
+            Flip();
+        }
+        else if (!isFacingRight && movementInputDirection > 0)
+        {
+            Flip();
+        }
+
+        if (body.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+    }
+
+    private void ResetPlayer()
+    {
+        if (body.position.y < gameObject.transform.position.y)
+        {
+            body.position = startPositions;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y, wallCheck.position.z));
+    }
+}
